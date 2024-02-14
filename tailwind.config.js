f8890e0b8423816905167b8b4f0248da52a95c39
MohsenArvan <f.n.raiat@gmail.com>
2024-02-14 17:33:38 +0330

    Refactor ForgotPassword.vue and add SvgBoard.svg and borderWidth in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,16 @@ export default {
             white: '#fff',
             
         },
+        borderWidth: {
+            DEFAULT: '1px',
+            '0': '0',
+            '0.5': '0.5px',
+            '2': '2px',
+            '3': '3px',
+            '4': '4px',
+            '6': '6px',
+            '8': '8px',
+          }
     },
 
     plugins: [forms],
