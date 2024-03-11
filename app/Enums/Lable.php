d9d97077cc869d0ef5f98e80a7f34b8025020abb
MohsenArvan <f.n.raiat@gmail.com>
2024-03-11 17:38:04 +0330

    Add Label, LabelTask, and TaskLabel models

---
+++ app/Enums/Lable.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace App\Enums;
+
+enum label: string {
+    case red = 'red';
+    case green = 'green';
+    case blue = 'blue';
+    case yellow = 'yellow';
+    case purple = 'purple';
+    case indigo = 'indigo';
+}
